bcfac31a874bef7506aa433fcd41fada39968f49
danielbanmeke <banmekedaniel@gmail.com>
2023-09-07 17:33:03 +0100

    first update

--- src/ForCounter.java
+++ src/ForCounter.java
@@ -3,5 +3,7 @@ public class ForCounter {
         for (int counter =1; counter <= 10; counter++)
             System.out.printf("%d  ", counter);
         System.out.println();
+        for (int i = 99; i >= 0; i -= 11)
+            System.out.printf("%d  ", i);
     }
 }
