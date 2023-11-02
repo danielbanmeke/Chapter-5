4e9d17a3849e0c8cde3e8a3b3d4a156f89879575
danielbanmeke <banmekedaniel@gmail.com>
2023-11-02 13:53:40 +0100

    Update all files

--- src/DoWhileLoop.java
+++ src/DoWhileLoop.java
@@ -1,9 +1,15 @@
+import java.util.Scanner;
+
 public class DoWhileLoop {
     public static void main(String[] args) {
-        int i = 1;
+        Scanner input = new Scanner(System.in);
+
+        int i;
         do {
-            System.out.println("Hello " + i);
-            i++;
-        } while(i <= 3);
+            System.out.print("Enter a number between 1 and 10: ");
+            i = input.nextInt();
+        } while(i < 1 || i > 10);
+
+        System.out.println(i + " is between 1 and 10");
     }
 }
