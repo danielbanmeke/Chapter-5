654fe6eaae9dcf6ce5b19266e8782b13b0568a88
danielbanmeke <banmekedaniel@gmail.com>
2023-11-02 13:26:48 +0100

    Update all files

--- src/WhileLoop.java
+++ src/WhileLoop.java
@@ -1,9 +1,16 @@
+import java.util.Scanner;
 public class WhileLoop {
     public static void main(String[] args) {
-        int i = 1;
-        while(i <= 5){
-            System.out.println("Hello");
-            i++;
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a number between 1 and 10: ");
+        int n = input.nextInt();
+        while(n >= 1 && n <= 10){
+            System.out.print(n + " is between 1 and 10.");
+
+            n = input.nextInt();
         }
+
+        System.out.println(n + " is not between 1 and 10. Try again later");
     }
 }
