baceab1e55ea60bd41115f9bfaffe00a98fc1f30
danielbanmeke <banmekedaniel@gmail.com>
2023-11-03 08:29:40 +0100

    Update all files

--- src/DecreasingEvenNumbers.java
+++ src/DecreasingEvenNumbers.java
@@ -1,12 +1,14 @@
-public class IncreasingEvenNumbers {
+import java.sql.SQLOutput;
+
+public class DecreasingEvenNumbers {
     public static void main(String[] args) {
 
         System.out.println("  ");
-        System.out.println("INCREASING EVEN NUMBERS BETWEEN 1 AND 100");
+        System.out.println("DECREASING EVEN NUMBER BETWEEN 1 AND 100");
         System.out.println("  ");
-
-        for (int n = 2; n <= 100; n++)
+        for (int n = 100; n >= 1; n--)
             if (n % 2 == 0)
                 System.out.print(n + " ");
+
     }
 }
