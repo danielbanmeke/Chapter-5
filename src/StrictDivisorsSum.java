a81048b28e2523f96303b97e66b89a15895300fb
danielbanmeke <banmekedaniel@gmail.com>
2023-11-06 15:04:25 +0100

    Update all files

--- src/StrictDivisorsSum.java
+++ src/StrictDivisorsSum.java
@@ -4,7 +4,15 @@ public class StrictDivisorsSum {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
+        System.out.print("Enter a number: ");
+        int n = input.nextInt();
 
+        int sum = 0;
+        for (int i = 1; i <= n/2; i++)
+            if (n % i == 0)
+                sum += i;
+
+        System.out.println(sum + n);
 
     }
 }
